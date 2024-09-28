655f14e8760025a10752bc7efeb282f7dbc4b4c8
Indoriajs <gaminglegendsultimate@gmail.com>
2024-09-28 23:01:04 +0530

    made more responsive

--- js/index.js
+++ js/index.js
@@ -70,3 +70,14 @@ function getStyleVariables() {
 }
 
 
+function sendEmail() {
+    message = document.getElementById("message").value
+    var link = "mailto:hindoria846@gmail.com"
+    + "?cc=gaminglegendsultimate@gmail.com"
+    + "&subject=" + encodeURIComponent("This is my subject")
+    + "&body=" + encodeURIComponent(message)
+    ;
+
+    console.log(message)
+    window.location.href = link;
+}
