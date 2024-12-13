90dec9e7b5b3ec71f640636e260e0ed53ffed8ac
HarryNguyenUSYD <nguyenhoangquan2122@gmail.com>
2024-12-14 01:32:28 +0700

    Harry: Fixed Navbar not sticky, implementing Homepage

--- app/src/Components/Navbar/Navbar.js
+++ app/src/Components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
     return (
-        <div className="top-0 sticky bg-black h-[10vh] w-full inline-flex">
+        <div className="fixed top-0 bg-black h-20 w-full inline-flex z-50">
             <img className="h-full w-auto relative" src="/resources/logo without tagline.png" alt="Company Logo"/>
-            <div className="h-full w-[50%] relative grid grid-cols-5 
-                         *:text-white *:flex *:justify-center *:items-center *:text-[2.5vmin]
+            <div className="h-full w-[55%] relative grid grid-cols-5 
+                         *:text-white *:flex *:justify-center *:items-center *:text-[1.5vw]
                          hover:*:bg-white hover:*:text-black *:duration-200">
                 <a href="/home">
                     <p>Home</p>
@@ -22,8 +22,8 @@ export default function Navbar() {
                 </a>
             </div>
 
-            <a href="/home#form" className="absolute right-[2vmin] top-[2vmin] w-[10vw] h-[calc(100%-4vmin)] rounded-full bg-[#debc6b]
-                                            flex flex-row justify-center items-center text-[2.5vmin] font-bold text-black duration-200 hover:bg-white">
+            <a href="/home#form" className="absolute right-[1rem] top-[1rem] w-[10vw] h-[calc(100%-2rem)] rounded-full bg-[#debc6b]
+                                            flex flex-row justify-center items-center text-[1.25vw] font-bold text-black duration-200 hover:bg-white">
                 <p>Contact Us</p>
             </a>
         </div>
--- app/src/Home/Home.js
+++ app/src/Home/Home.js
@@ -3,9 +3,10 @@ import Navbar from "../Components/Navbar/Navbar";
 
 export default function Home() {
     return (
-        <div className="bg-blue-400 overflow-x-hidden">
+        <div className="bg-white overflow-x-hidden">
             <Navbar />
             {titlePage()}
+            {projectsPage()}
             <Footer />
         </div>
     );
@@ -14,7 +15,44 @@ export default function Home() {
 function titlePage() {
     return (
         <div className="h-screen w-screen">
-            <div className="absolute w-auto h-auto"></div>
+            <div className="absolute left-0 top-[5vh] w-1/2 h-screen
+                            flex flex-col justify-center items-center gap-[3vh]">
+                <p className="font-serif text-black text-[6vw]">TELES TECH</p>
+                <p className="w-[80%] text-black text-[2vw] text-center">We build quality applications for businesses that scales like a dream</p>
+                <div className="flex flex-row justify-center items-center gap-[1vw] pt-[2vh]">
+                    <button className="rounded-full bg-[#977f48] w-auto h-auto flex flex-row px-[2vw] py-[0.5vw]
+                                    justify-center items-center text-[1.5vw] font-bold text-black duration-200 hover:bg-black hover:text-white">
+                        Get Started
+                    </button>
+                    <button className="rounded-full bg-[#debc6b] w-auto h-auto flex flex-row px-[2vw] py-[0.5vw]
+                                    justify-center items-center text-[1.5vw] font-bold text-black duration-200 hover:bg-black hover:text-white">
+                        Contact Us
+                    </button>
+                </div>
+                
+            </div>
+
+            <div className="absolute right-0 top-[5vh] w-1/2 h-screen">
+                <div className="absolute w-full h-full flex justify-center items-center">
+                    <img src="/resources/just_the_T[1].png" className="absolute w-full h-auto p-[20%]" alt="Company Logo" />
+                </div>
+                <div className="absolute w-full h-full flex justify-center items-center">
+                    <div className="absolute w-full h-full bg-gradient-to-tl from-white from-25% to-transparent"></div>
+                </div>
+            </div>
         </div>
     )
+}
+
+function projectsPage() {
+    return (
+        <div className="relative h-screen w-screen">
+            <div className="absolute w-screen h-1/2 top-0 left-0 border-[2vw] border-white border-r-[3vw]
+                            flex justify-center items-center">
+                <img className="absolute w-full h-full brightness-75"
+                     src="/resources/240_F_885937882_GqcwdIttrGepx8ESiItGBG44tOvopC4K.jpg" alt="background" />
+                <p className="text-[4vw] text-white z-10">Our Projects</p>
+            </div>
+        </div>
+    );
 }
