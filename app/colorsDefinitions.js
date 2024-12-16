e9be16440ed07d807c798601fc9d91b5599fc9fa
HarryNguyenUSYD <nguyenhoangquan2122@gmail.com>
2024-12-16 15:16:45 +0700

    Transferring to branch Harry

--- app/colorsDefinitions.js
+++ app/colorsDefinitions.js
@@ -1,8 +1,7 @@
 const OurColors = {
     'background-blue': '#1A54A9',
-    'logo-yellow': '#debc6b',
     'dark-yellow': '#977f48',
-    ogoyellow: '#debc6b',
+    logoyellow: '#debc6b',
     logodarkyellow: '#977f48',
     enquirygray: 'rgb(209 213 219)',
     footergray: 'rgb(107 114 128)'
